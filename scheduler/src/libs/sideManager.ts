7163810ca2f2ea0ff0a70a80c37980c5b51eff7c
hyperthreading <no1expman@gmail.com>
2018-06-24 02:02:02 +0900

    Fix mkdir error on multiple side files

--- scheduler/src/libs/sideManager.ts
+++ scheduler/src/libs/sideManager.ts
@@ -13,34 +13,38 @@ const copyFile = util.promisify(fs.copyFile);
 
 // testDir will be created
 export function writeTests(sideDir: string, testDir: string) {
-  return Promise.all(
-    fs
-      .readdirSync(sideDir)
-      .filter(fileName => path.extname(fileName).endsWith(".side"))
-      .map(fileName => {
-        const sideContent = readSide(path.join(sideDir, fileName));
-        const projectName = path.basename(fileName, ".side");
-        const projectDir = path.join(testDir, projectName);
-        return mkdir(testDir)
-          .then(() => initDirectory(projectDir))
-          .then(() =>
-            copyFile(
-              path.join(sideDir, projectName + ".side"),
-              path.join(projectDir, projectName + ".side")
+  return mkdir(testDir).then(() =>
+    Promise.all(
+      fs
+        .readdirSync(sideDir)
+        .filter(fileName => path.extname(fileName).endsWith(".side"))
+        .map(fileName => {
+          const sideContent = readSide(path.join(sideDir, fileName));
+          const projectName = path.basename(fileName, ".side");
+          const projectDir = path.join(testDir, projectName);
+          return initDirectory(projectDir)
+            .then(() =>
+              copyFile(
+                path.join(sideDir, projectName + ".side"),
+                path.join(projectDir, projectName + ".side")
+              )
             )
-          )
-          .then(() => sideContent)
-          .then((project: Project) => {
-            return Promise.all(
-              project.code.map(suiteCode => {
-                const suiteName = suiteCode.name;
-                const suitePath = path.join(projectDir, suiteName) + ".test.js";
-                return writeFile(suitePath, beautify(suiteCode.code));
-              })
-            );
-          })
-          .then(() => { console.log('Wrote test for', projectName)});
-      })
+            .then(() => sideContent)
+            .then((project: Project) => {
+              return Promise.all(
+                project.code.map(suiteCode => {
+                  const suiteName = suiteCode.name;
+                  const suitePath =
+                    path.join(projectDir, suiteName) + ".test.js";
+                  return writeFile(suitePath, beautify(suiteCode.code));
+                })
+              );
+            })
+            .then(() => {
+              console.log("Wrote test for", projectName);
+            });
+        })
+    )
   );
 }
 
